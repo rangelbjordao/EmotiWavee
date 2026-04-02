a6f563ee129206a25227954aeb21788997185ed4
Rangel <96200914+rangelbjordao@users.noreply.github.com>
2026-04-02 16:34:30 -0300

    Update SpotifyClient.java

--- src/main/java/org/example/emotiwave/infra/client/SpotifyClient.java
+++ src/main/java/org/example/emotiwave/infra/client/SpotifyClient.java
@@ -3,34 +3,30 @@ package org.example.emotiwave.infra.client;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.Nullable;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-import java.util.Optional;
-
 import org.example.emotiwave.application.dto.out.AccessTokenResponseDto;
 import org.example.emotiwave.domain.entities.Usuario;
-import org.example.emotiwave.domain.exceptions.FalhaAoPegarTokenAcess;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Optional;
+
 @Component
 public class SpotifyClient {
     private final ObjectMapper objectMapper;
-    private final String redirectUri = "http://192.168.15.58:8080/spotify/callback";
     private final String scopes = "user-top-read user-read-private user-read-recently-played";
-    private final String baseUrl = "https://accounts.spotify.com/authorize";
-    private final RestTemplate restTemplate = new RestTemplate();
     private final WebClient webClient;
+    @Value("${spotify.redirect-uri}")
+    private String redirectUri;
     @Value("${spotify.client-id}")
     String clientId;
     @Value("${spotify.client-secret}")
@@ -43,19 +39,24 @@ public class SpotifyClient {
 
     public String construirAutorizacao(String authHeader) {
         String jwt = authHeader.replace("Bearer ", "");
-        return UriComponentsBuilder.fromHttpUrl("https://accounts.spotify.com/authorize").queryParam("client_id", new Object[]{this.clientId}).queryParam("response_type", new Object[]{"code"}).queryParam("redirect_uri", "http://192.168.15.58:8080/spotify/callback").queryParam("scope", new Object[]{"user-top-read user-read-private user-read-recently-played"}).queryParam("state", new Object[]{jwt}).build().toUriString();
+
+        return UriComponentsBuilder.fromHttpUrl("https://accounts.spotify.com/authorize").queryParam("client_id", this.clientId).queryParam("response_type", "code").queryParam("redirect_uri", redirectUri).queryParam("scope", scopes).queryParam("state", jwt).build().toUriString();
     }
 
     public AccessTokenResponseDto exchangeCodeForTokens(String code) {
         try {
             String basicAuth = Base64.getEncoder().encodeToString((this.clientId + ":" + this.secret).getBytes(StandardCharsets.UTF_8));
+
             MultiValueMap<String, String> formData = new LinkedMultiValueMap();
             formData.add("grant_type", "authorization_code");
             formData.add("code", code);
-            formData.add("redirect_uri", "http://192.168.15.58:8080/spotify/callback");
-            WebClient webClient = WebClient.builder().baseUrl("https://accounts.spotify.com").exchangeStrategies(ExchangeStrategies.builder().codecs((configurer) -> configurer.defaultCodecs().maxInMemorySize(Integer.MAX_VALUE)).build()).build();
-            return (AccessTokenResponseDto)((WebClient.RequestBodySpec)((WebClient.RequestBodySpec)webClient.post().uri("/api/token", new Object[0])).header("Authorization", new String[]{"Basic " + basicAuth})).contentType(MediaType.APPLICATION_FORM_URLENCODED).bodyValue(formData).retrieve().bodyToMono(AccessTokenResponseDto.class).block();
-        } catch (FalhaAoPegarTokenAcess e) {
+            formData.add("redirect_uri", redirectUri);
+
+            WebClient webClient = WebClient.builder().baseUrl("https://accounts.spotify.com").build();
+
+            return webClient.post().uri("/api/token").header("Authorization", "Basic " + basicAuth).contentType(MediaType.APPLICATION_FORM_URLENCODED).bodyValue(formData).retrieve().bodyToMono(AccessTokenResponseDto.class).block();
+
+        } catch (Exception e) {
             throw new RuntimeException("Erro ao trocar código por tokens do Spotify", e);
         }
     }
@@ -67,7 +68,7 @@ public class SpotifyClient {
             MultiValueMap<String, String> formData = new LinkedMultiValueMap();
             formData.add("grant_type", "refresh_token");
             formData.add("refresh_token", URLEncoder.encode(refreshToken, StandardCharsets.UTF_8));
-            return (AccessTokenResponseDto)((WebClient.RequestBodySpec)((WebClient.RequestBodySpec)this.webClient.post().uri("https://accounts.spotify.com/api/token", new Object[0])).header("Authorization", new String[]{"Basic " + basicAuth})).contentType(MediaType.APPLICATION_FORM_URLENCODED).bodyValue(formData).retrieve().bodyToMono(AccessTokenResponseDto.class).block();
+            return (AccessTokenResponseDto) ((WebClient.RequestBodySpec) ((WebClient.RequestBodySpec) this.webClient.post().uri("https://accounts.spotify.com/api/token", new Object[0])).header("Authorization", new String[]{"Basic " + basicAuth})).contentType(MediaType.APPLICATION_FORM_URLENCODED).bodyValue(formData).retrieve().bodyToMono(AccessTokenResponseDto.class).block();
         } catch (Exception e) {
             throw new RuntimeException("Erro ao renovar access token do Spotify", e);
         }
@@ -76,7 +77,7 @@ public class SpotifyClient {
     public <T> T enviarRequisicaoSpotifyUtils(Usuario usuario, String url, ParameterizedTypeReference<T> responseType, @Nullable Long after) {
         try {
             String urlFinal = UriComponentsBuilder.fromHttpUrl(url).queryParamIfPresent("after", Optional.ofNullable(after)).toUriString();
-            return (T)this.webClient.get().uri(urlFinal, new Object[0]).header("Authorization", new String[]{"Bearer " + usuario.getSpotifyInfo().getAccessToken()}).retrieve().bodyToMono(responseType).block();
+            return (T) this.webClient.get().uri(urlFinal, new Object[0]).header("Authorization", new String[]{"Bearer " + usuario.getSpotifyInfo().getAccessToken()}).retrieve().bodyToMono(responseType).block();
         } catch (WebClientResponseException e) {
             throw new RuntimeException("Falha ao consultar Spotify: " + String.valueOf(e.getStatusCode()) + " - " + e.getResponseBodyAsString(), e);
         } catch (Exception e) {
