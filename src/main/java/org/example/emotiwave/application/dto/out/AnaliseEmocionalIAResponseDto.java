1fff7102beb74d9078f5a322d03b3128e7d1abef
Rangel <96200914+rangelbjordao@users.noreply.github.com>
2026-04-07 18:39:44 -0300

    .

--- src/main/java/org/example/emotiwave/application/dto/out/AnaliseEmocionalIAResponseDto.java
+++
@@ -1,9 +0,0 @@
-package org.example.emotiwave.application.dto.out;
-
-public record AnaliseEmocionalIAResponseDto(
-        double humorMedio,
-        String tendencia,
-        String estado,
-        String atividadeRelacionada,
-        String recomendacao
-) {}
